66bb39d36b845c8ce77210094236540ef0beda45
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2020-10-25 07:55:21 +0100

    Added ZoKrates circuit compilation script.

---
+++ zokrates/compile_circuit_and_make_setup.sh
@@ -0,0 +1,7 @@
+set -e; set -x
+ZOKRATES_HOME="${ZOKRATES_HOME:-~/fork/Zokrates/zokrates_stdlib/stdlib}"
+ZOKRATES_BIN="${ZOKRATES_BIN:-$ZOKRATES_HOME/../../target/release/zokrates}"
+rm -f out out.ztf proving.key verification.key abi.json log >/dev/null
+${ZOKRATES_BIN} check --input legalage.zok > log 2>&1
+${ZOKRATES_BIN} compile --input legalage.zok >> log 2>&1
+${ZOKRATES_BIN} setup >> log 2>&1
